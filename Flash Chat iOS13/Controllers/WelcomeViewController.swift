498e421eed09a617f98f599df4960d649eb2c70a
Muhammad Umar Farisi <pharese@gmail.com>
2020-07-14 21:17:12 +0700

    remove CLType library and change the bundle id

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,13 +15,7 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let text = "⚡️FlashChat"
-        var index = 0
-        for letter in text {
-            Timer.scheduledTimer(withTimeInterval: 0.1 * Double(index), repeats: false) { (time) in
-                self.titleLabel.text?.append(letter)
-            }
-            index += 1
-        }
+        titleLabel.text = text
     }
     
 
